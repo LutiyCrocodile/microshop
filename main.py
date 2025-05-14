709ba3613ce8685d7db0837f434c4580f6d48de6
LutiyCrocodile <morentroshin@yandex.ru>
2025-05-14 09:57:03 +0300

    alembic

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from contextlib import asynccontextmanager
 import uvicorn
 from fastapi import FastAPI
 
-from core.models import Base, db_helper
 from core.config import settings
 from api_v1 import router as router_v1
 from items_views import router as items_router
@@ -12,8 +11,6 @@ from users.views import router as users_router
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    async with db_helper.engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
     yield
 
 
